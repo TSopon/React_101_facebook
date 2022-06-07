8bc556c25eb01f6fb033141ca599f53713159e30
TSopon <sopon101n@gmail.com>
2022-06-07 21:28:09 +0700

    add deletedPost

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './Navbar';
 import Input from './Input';
 import Post from './Post';
+import { func } from 'prop-types';
 
 let id = 1;
 
@@ -15,12 +16,18 @@ function App() {
     setPosts([newPost, ...posts]);
     id += 1;
   }
+
+  function deletePost(id) {
+    const updatedPosts = posts.filter((post) => post.id !== id );
+    setPosts(updatedPosts);
+  }
+
   return (
     <div className="App">
       <Navbar />
       <Input addPost={addPost}/>
       {posts.map((post)=> (
-        <Post key={post.id} id={post.id} title={post.title}/>
+        <Post key={post.id} id={post.id} title={post.title} deletePost={deletePost}/>
       ))}
     </div>
   );
